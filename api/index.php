04a26058a917d0aab1f9db50e41e66c89b2ed56b
Elias Tapias <it.systems.hub@gmail.com>
2026-04-16 15:32:50 -0400

    SUBIR

--- api/index.php
+++ api/index.php
@@ -1,8 +1,9 @@
 <?php
 
 /**
- * Zenith ERP - Vercel Bridge
- * Procesa las peticiones entrantes de Vercel y las redirige al núcleo de Laravel.
+ * Vercel Proxy for Laravel
+ * Este archivo permite que Vercel ejecute el motor de Laravel correctamente
+ * al ser el punto de entrada principal del runtime de PHP.
  */
 
 require __DIR__ . '/../public/index.php';
